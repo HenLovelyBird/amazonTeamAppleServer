66df3c63ea43a1c74a680888fbefec525941a8ac
jeffascript <ofoaro.jeff@gmail.com>
2019-12-13 12:09:42 +0100

    delete and put added

--- src/products/index.js
+++ src/products/index.js
@@ -15,4 +15,58 @@ router.get('/', async (req, res)=> {
 });
 
 
+
+
+
+const readAllFile = async ()=>{ 
+    const buffer = await readFile(filePath)  
+    const content = buffer.toString();
+    return JSON.parse(content)
+  }
+
+
+router.put("/:id", async (req, res) =>{
+    let allProducts = await readAllFile();
+
+    let product = allProducts.find(oneProduct => oneProduct._id == req.params.id)
+    if (product){
+        let mergedProduct = Object.assign(product, req.body)
+        let postionOfProduct = allProducts.indexOf(product)
+        allProducts[postionOfProduct] = mergedProduct
+        await writeFile(filePath, JSON.stringify(this.allProducts));
+        res.send(mergedProduct);
+
+    }
+
+    else{
+        res.status(404).send("Not Found bro...damn it!!!") 
+
+}
+
+
+})
+
+
+
+
+
+
+router.delete("/:id", async (req,res)=>{
+   let allProducts = await readAllFile();
+    let productsToRemain = allProducts.filter(product => product._id != req.params.id)
+
+    if(productsToRemain.length < allProducts.length ){
+       await writeFile(filePath, JSON.stringify(productsToRemain));
+       res.send("removed") 
+
+    }
+
+    else{
+        res.status(404).send("Not Found...damn it!!!") 
+    }
+
+
+})
+
+
 module.exports = router;
