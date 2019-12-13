2b8ce25af2ec84ca7b63d05213efe17651fc0091
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-13 12:08:03 +0100

    prettier

--- src/products/index.js
+++ src/products/index.js
@@ -1,19 +1,18 @@
 const express = require("express");
-const {readFile, writeFile} = require ("fs-extra");
-const uuidv1 = require('uuid/v1');
-var multer  = require('multer')
+const { readFile, writeFile } = require("fs-extra");
+const uuidv1 = require("uuid/v1");
+var multer = require("multer");
 const path = require("path");
-const {check,validationResult, sanitize} = require("express-validator");
+const { check, validationResult, sanitize } = require("express-validator");
 const router = express.Router();
 const filePath = path.join(__dirname, "products.json");
 
-
-router.get('/', async (req, res)=> {
-    const buffer = await readFile(filePath);
-    const fileContent = buffer.toString();
-    const productsArray = JSON.parse(fileContent);
-    // console.log(productArray);
-    res.send(productsArray);
+router.get("/", async (req, res) => {
+  const buffer = await readFile(filePath);
+  const fileContent = buffer.toString();
+  const productsArray = JSON.parse(fileContent);
+  // console.log(productArray);
+  res.send(productsArray);
 });
 
 var upload = multer({});
@@ -31,6 +30,5 @@ router.post("/", upload.none(), async (req, res) => {
   await writeFile(filePath, newProductbuffer);
   res.send(newProduct);
 });
-
 
 module.exports = router;
