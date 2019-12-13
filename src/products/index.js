1d37818a8d57a97373d98c271d81163643fd97db
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-13 11:41:23 +0100

    Post Product without img

--- src/products/index.js
+++ src/products/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const {readFile, writeFile} = require ("fs-extra");
+const uuidv1 = require('uuid/v1');
+var multer  = require('multer')
 const path = require("path");
 const {check,validationResult, sanitize} = require("express-validator");
 const router = express.Router();
@@ -12,6 +14,22 @@ router.get('/', async (req, res)=> {
     const productsArray = JSON.parse(fileContent);
     // console.log(productArray);
     res.send(productsArray);
+});
+
+var upload = multer({});
+router.post("/", upload.none(), async (req, res) => {
+  const buffer = await readFile(filePath);
+  const fileContent = buffer.toString();
+  const productsArray = JSON.parse(fileContent);
+  let newProduct = {
+    ...req.body,
+    _id: uuidv1(),
+    createdAt: new Date()
+  };
+  productsArray.push(newProduct);
+  const newProductbuffer = JSON.stringify(productsArray);
+  await writeFile(filePath, newProductbuffer);
+  res.send(newProduct);
 });
 
 
