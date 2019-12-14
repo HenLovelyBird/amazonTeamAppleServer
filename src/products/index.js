1de89e4dc0ed0f1e226aec160a8763650ae98180
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-14 01:49:32 +0100

    merge fixes

--- src/products/index.js
+++ src/products/index.js
@@ -7,110 +7,90 @@ const { check, validationResult, sanitize } = require("express-validator");
 const router = express.Router();
 const filePath = path.join(__dirname, "products.json");
 
-
-
-router.get('/', async (req, res)=> {
-    const buffer = await readFile(filePath);
-    const fileContent = buffer.toString();
-    const productsArray = JSON.parse(fileContent);
-    // console.log(productsArray);
-    res.send(productsArray);
-});
-
-router.get('/:id', async (req, res)=>{
-    const buffer = await readFile(filePath);
-    const fileContent = buffer.toString();
-    const productsArray = JSON.parse(fileContent);
-    const findProduct = productsArray.find(
-        product => product._id === req.params.id
-    );
-    // console.log(req.params.id);
-    if (findProduct){
-        res.send(findProduct);
-    } else {
-        res.status(401).send(`product ${req.params.id} not found!`);
-    }
-});  
-
-var upload = multer({});
-router.post("/", upload.none(), async (req, res) => {
+router.get("/", async (req, res) => {
   const buffer = await readFile(filePath);
   const fileContent = buffer.toString();
   const productsArray = JSON.parse(fileContent);
-  let newProduct = {
-    ...req.body,
-    _id: uuidv1(),
-    createdAt: new Date()
-  };
-  productsArray.push(newProduct);
-  const newProductbuffer = JSON.stringify(productsArray);
-  await writeFile(filePath, newProductbuffer);
-  res.send(newProduct);
-
+  res.send(productsArray);
 });
 
-
-
-
-
-const readAllFile = async ()=>{ 
-    const buffer = await readFile(filePath)  
-    const content = buffer.toString();
-    return JSON.parse(content)
+router.get("/:id", async (req, res) => {
+  const buffer = await readFile(filePath);
+  const fileContent = buffer.toString();
+  const productsArray = JSON.parse(fileContent);
+  const findProduct = productsArray.find(
+    product => product._id === req.params.id
+  );
+  if (findProduct) {
+    res.send(findProduct);
+  } else {
+    res.status(401).send(`product ${req.params.id} not found!`);
   }
+});
 
+var upload = multer({});
+router.post("/", upload.single("productImg"), async (req, res) => {
+  const newID = uuidv1();
+  const { originalname, buffer } = req.file;
+  const ext = path.extname(originalname);
+  const ImgNameToBeSaved = newID.concat(ext);
+  let ImgFilePath = path.join(__dirname, "img", ImgNameToBeSaved);
+  await writeFile(ImgFilePath, buffer);
+  let allProducts = await readAllFile();
+  let newProduct = {
+    ...req.body,
+    image: ImgFilePath,
+    _id: newID,
+    createdAt: new Date()
+  };
+  allProducts.push(newProduct);
+  const newProductbuffer = JSON.stringify(allProducts);
+  await writeFile(filePath, newProductbuffer);
+  res.send(newProduct);
+});
 
-router.put("/:id", async (req, res) =>{  
+router.put("/:id", upload.single("productImg"), async (req, res) => {
+  let allProducts = await readAllFile();
+  let product = allProducts.find(oneProduct => oneProduct._id == req.params.id);
+  if (product) {
+    const { originalname, buffer } = req.file;
+    const ext = path.extname(originalname);
+    const ImgNameToBeSaved = req.params.id.concat(ext);
+    let ImgFilePath = path.join(__dirname, "img", ImgNameToBeSaved);
+    await writeFile(ImgFilePath, buffer);
+    let updatedProduct = {
+      ...req.body,
+      image: ImgFilePath,
+      updatedAt: new Date()
+    };
+    let mergedProduct = Object.assign(product, updatedProduct);
+    let postionOfProduct = allProducts.indexOf(product);
+    allProducts[postionOfProduct] = mergedProduct;
+    await writeFile(filePath, JSON.stringify(allProducts));
+    res.send(mergedProduct);
+  } else {
+    res.status(404).send("Not Found bro...damn it!!!");
+  }
+});
 
-    var allProducts = await readAllFile();
+const readAllFile = async () => {
+  const buffer = await readFile(filePath);
+  const content = buffer.toString();
+  return JSON.parse(content);
+};
 
-    let product = allProducts.find(oneProduct => oneProduct._id == req.params.id);
-    if (product){
-       
-        let newProduct = {
-            ...req.body,
-           
-            UpdatedAt: new Date()
-          };
+router.delete("/:id", async (req, res) => {
+  let allProducts = await readAllFile();
+  let productsToRemain = allProducts.filter(
+    product => product._id != req.params.id
+  );
 
-         let mergedProduct = Object.assign(product, newProduct);
-         let postionOfProduct = allProducts.indexOf(product);
-         allProducts[postionOfProduct] = mergedProduct;
-
-         
-         await writeFile(filePath, JSON.stringify(allProducts));         
-         res.send(mergedProduct);
-
-    }
-     else{
-        res.status(404).send("Not Found bro...damn it!!!");
- }
-
- })
-
-
-
-
-
-
-router.delete("/:id", async (req,res)=>{
-   let allProducts = await readAllFile();
-    let productsToRemain = allProducts.filter(product => product._id != req.params.id)
-
-    if(productsToRemain.length < allProducts.length ){
-       await writeFile(filePath, JSON.stringify(productsToRemain));
-       res.send("removed") 
-
-    }
-
-    else{
-        res.status(404).send("Not Found...damn it!!!") 
-    }
-
-
-})
-
+  if (productsToRemain.length < allProducts.length) {
+    await writeFile(filePath, JSON.stringify(productsToRemain));
+    res.send("removed");
+  } else {
+    res.status(404).send("Not Found...damn it!!!");
+  }
+});
 
 module.exports = router;
-
-
--- src/reviews/index.js
+++ src/reviews/index.js
@@ -2,108 +2,84 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const filePath = path.join(__dirname, "reviews.json");
-const { readFile, writeFile} = require("fs-extra");
+const { readFile, writeFile } = require("fs-extra");
 const uuidv1 = require("uuid/v1");
 
+const readAllFile = async () => {
+  const buffer = await readFile(filePath);
+  const content = buffer.toString();
+  return JSON.parse(content);
+};
 
-const readAllFile = async ()=>{ 
-    const buffer = await readFile(filePath)  
-    const content = buffer.toString();
-    return JSON.parse(content)
-  }
-
-router.get('/', async (req, res)=> {
-    const reviewsArray = await readAllFile();
-    res.send(reviewsArray);
+router.get("/", async (req, res) => {
+  const reviewsArray = await readAllFile();
+  res.send(reviewsArray);
 });
 
+router.get("/:id", async (req, res) => {
+  const reviewsArray = await readAllFile();
+  const findReview = reviewsArray.find(review => review._id === req.params.id);
 
-router.get('/:id', async (req, res)=>{
-    const reviewsArray = await readAllFile();
-    const findReview = reviewsArray.find(
-        review => review._id === req.params.id
-    );
-    
-    if (findReview){
-        res.send(findReview);
-    } else {
-        res.status(401).send(`product ${req.params.id} not found!`);
-    }
-});  
-
+  if (findReview) {
+    res.send(findReview);
+  } else {
+    res.status(401).send(`product ${req.params.id} not found!`);
+  }
+});
 
 // (GET /products/{id}/Reviews)
 
 router.get("/product/:productID", async (req, res) => {
+  const reviewsArray = await readAllFile();
 
-      const reviewsArray = await readAllFile();
+  const reviewElementId = reviewsArray.filter(
+    review => review.elementId === req.params.productID
+  );
 
-      const reviewElementId = reviewsArray.filter(
-        review => review.elementId === req.params.productID)
-
-      if (reviewElementId) {
-       
-        res.send(reviewElementId);
-      } else {
-        res.status(401).send(`product with element ID ${req.params.id} not found!`);
-      }
-    })
- 
-
-
-
-
-
-
-
-
-
-
-
-
-router.delete("/:id", async (req,res)=>{
-    let allReviews = await readAllFile();
-     let reviewsToRemain = allReviews.filter(review => review._id != req.params.id)
- 
-     if(reviewsToRemain.length < allReviews.length ){
-        await writeFile(filePath, JSON.stringify(reviewsToRemain));
-        res.send("removed") 
- 
-     }
- 
-     else{
-         res.status(404).send("Not Found...damn it!!!") 
-     }
- 
- 
- })
-
-
-
-
-router.put('/:id', async (req, res) => {
-    const modifyReview = req.body;
-    const buffer = await readFile(filePath);
-    const fileContent = buffer.toString();
-    let reviewsArray = JSON.parse(fileContent);
-    await writeFile(filePath, JSON.stringify(reviewsArray));
-    res.send(modifyReview);
+  if (reviewElementId.length > 0) {
+    res.send(reviewElementId);
+  } else {
+    res
+      .status(401)
+      .send(`product with element ID ${req.params.productID} not found!`);
+  }
 });
 
-router.post('/', async (req, res) =>{
-    const buffer = await readFile(filePath);
-    const fileContent = buffer.toString();
-    let reviewsArray = JSON.parse(fileContent);
-    const newReview = {
-        ...req.body,
-        _id: uuidv1(), 
-        createdat: new Date()
-    };
-    reviewsArray.push(newReview);
-    await writeFile(filePath, JSON.stringify(reviewsArray));
-    res.status(201).send(`${newReview._id}`);
+router.delete("/:id", async (req, res) => {
+  let allReviews = await readAllFile();
+  let reviewsToRemain = allReviews.filter(
+    review => review._id != req.params.id
+  );
+
+  if (reviewsToRemain.length < allReviews.length) {
+    await writeFile(filePath, JSON.stringify(reviewsToRemain));
+    res.send("removed");
+  } else {
+    res.status(404).send("Not Found...damn it!!!");
+  }
 });
 
+router.put("/:id", async (req, res) => {
+  const modifyReview = req.body;
+  const buffer = await readFile(filePath);
+  const fileContent = buffer.toString();
+  let reviewsArray = JSON.parse(fileContent);
+  await writeFile(filePath, JSON.stringify(reviewsArray));
+  res.send(modifyReview);
+});
 
+router.post("/", async (req, res) => {
+  const buffer = await readFile(filePath);
+  const fileContent = buffer.toString();
+  let reviewsArray = JSON.parse(fileContent);
+  const newReview = {
+    ...req.body,
+    _id: uuidv1(),
+    createdat: new Date()
+  };
+  reviewsArray.push(newReview);
+  await writeFile(filePath, JSON.stringify(reviewsArray));
+  res.status(201).send(`${newReview._id}`);
+});
 
 module.exports = router;
